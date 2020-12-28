0a7bc3041618176b0d9aaaa0652f13538feb2984
npyl <ulcheats@gmail.com>
2020-12-29 01:04:14 +0200

    Add login screen

---
+++ scripts/init-firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB37xSmEXteSUAyUdkrV4W_hVjyk0dsETY",
+    authDomain: "e-radio-fk.firebaseapp.com",
+    projectId: "e-radio-fk",
+    storageBucket: "e-radio-fk.appspot.com",
+    messagingSenderId: "152785870975",
+    appId: "1:152785870975:web:3a9ea657cbd248e5e95f4a",
+    measurementId: "G-1F2JYGKBEQ"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
