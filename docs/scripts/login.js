fe08774a11419592345ca8ee1d2d9540151ab849
npyl <ulcheats@gmail.com>
2020-12-30 00:13:31 +0200

    same

--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -9,6 +9,16 @@ function show_login_box() {
     document.getElementById('sign-in-text').style.visibility = 'hidden';
 }
 
+function hide_login_box() {
+    document.getElementById('login-box').style.visibility = 'hidden';
+    var nodes = document.getElementById('login-box').children;
+    for(var i = 0; i < nodes.length; i++) {
+        nodes[i].style.visibility = 'hidden';
+    }
+    document.getElementById('sign-in-button').style.visibility = 'visible';
+    document.getElementById('sign-in-text').style.visibility = 'visible';
+}
+
 function sign_in() {
     // enable login box
     show_login_box();
