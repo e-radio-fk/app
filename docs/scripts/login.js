0a869a799801c520765d1d4691b59db2e1684b31
npyl <ulcheats@gmail.com>
2020-12-30 00:46:33 +0200

    fix banner stepping under

--- docs/scripts/login.js
+++ docs/scripts/login.js
@@ -1,5 +1,6 @@
 function show_login_box() {
     document.getElementById('login-box').style.visibility = 'visible';
+    
     var nodes = document.getElementById('login-box').children;
     for(var i = 0; i < nodes.length; i++) {
         nodes[i].style.visibility = 'visible';
@@ -8,13 +9,14 @@ function show_login_box() {
     document.getElementById('sign-in-button').style.visibility = 'hidden';
     document.getElementById('sign-in-text').style.visibility = 'hidden';
 }
-
 function hide_login_box() {
     document.getElementById('login-box').style.visibility = 'hidden';
+    
     var nodes = document.getElementById('login-box').children;
     for(var i = 0; i < nodes.length; i++) {
         nodes[i].style.visibility = 'hidden';
     }
+    
     document.getElementById('sign-in-button').style.visibility = 'visible';
     document.getElementById('sign-in-text').style.visibility = 'visible';
 }
@@ -22,4 +24,8 @@ function hide_login_box() {
 function sign_in() {
     // enable login box
     show_login_box();
+}
+
+function sign_up() {
+
 }
