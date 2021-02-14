4630c2c870dc38dc4426e365931fd27ff632b37b
npyl <n.pylarinos@hotmail.com>
2021-02-14 12:33:48 +0200

    Add watchify

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-# less to css
-./node_modules/less/bin/lessc docs/less/main.less docs/css/main.css
+#!/bin/bash
 
-# now run webpack to ensure conversion of Node.js plugins to normal code
-browserify docs/_browserify/socket.io.js -o docs/scripts/socket.io.js
+npm run build
+npm run watch
