035245414840ed7d1b6cb77d5f1712139a302909
npyl <ulcheats@gmail.com>
2021-03-07 01:40:26 +0200

    test some server code

--- docs/server/index.js
+++ docs/server/index.js
@@ -1,26 +1,11 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const io = require("socket.io-client")(3000, {
+    path: "/stream",
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+io.on("connection", (socket) => {
+    socket.emit("hi", "everyone");
 });
 
-// const io = require("socket.io-client")(3000, {
-//     path: "/stream",
-// });
-
-// io.on("connection", (socket) => {
-//     socket.emit("hi", "everyone");
-// });
-
-// io.on("disconnect", (reason) => {
-//     console.log(reason);
-// });
+io.on("disconnect", (reason) => {
+    console.log(reason);
+});
